68723156512295a02aeedfb2229b4da169872dd3
xcheche <chekwubechukwu23@gmail.com>
2023-12-14 15:42:54 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'app_state.dart';
 
 import 'home_screen.dart';
 
-void main() => runApp(const App());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: (context, child) => const App(),
+    ),
+  );
+}
 
 class App extends StatelessWidget {
   const App({super.key});
